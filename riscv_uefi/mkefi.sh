dc8ecd9c3c8b2ad93820b9a47465c7250775b63e
lbbxsxlz <lbbxsxlz@gmail.com>
2023-09-21 18:29:31 +0800

    add scripts to make efi image
    Signed-off-by: lbbxsxlz <lbbxsxlz@gmail.com>

--- riscv_uefi/mkefi.sh
+++ riscv_uefi/mkefi.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
 WORK=`pwd`
-mkfs.msdos -C efi2.img 65536
-DEV=$(losetup -f)
-sudo losetup $DEV efi2.img
-sudo mount $DEV /mnt
-sudo cp $WORK/linux/arch/riscv/boot/Image /mnt
-#sudo cp ./rootfs.cpio /mnt
-sudo umount /mnt
-sudo losetup -d $DEV
+
+fallocate -l 512M efi.img
+sgdisk -n 1:34: -t 1:EF00 $WORK/efi.img
+
+sudo losetup -fP $WORK/efi.img
+loopdev=`losetup -j $WORK/efi.img | awk -F: '{print $1}'`
+efi_part="$loopdev"p1
+sudo mkfs.msdos $efi_part
+mkdir -p /tmp/mnt
+sudo mount $efi_part /tmp/mnt/
+sudo cp $WORK/linux/arch/riscv/boot/Image /tmp/mnt/
+sudo umount /tmp/mnt
+sudo losetup -D $loopdev
